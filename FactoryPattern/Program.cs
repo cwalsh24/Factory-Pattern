94b41a62b16973ea39e48ad8d1c196f961b6a599
cwalsh24 <cwalsh24@ut.utm.edu>
2023-01-26 14:07:49 -0600

    Add project files.

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    interface GeometricShape
+    {
+        void draw(); 
+    }
+    class ShapeFactory
+    {
+        GeometricShape getShape(GeometricShape shape)
+        {
+            
+        }
+    }
+
+    class Line : GeometricShape
+    {
+        draw(); 
+    }
+
+    class Circle
+    {
+        draw(); 
+    }
+
+    class Rectangle
+    {
+        draw(); 
+    }
+
+    enum ShapeType
+    {
+        Line,
+        Circle,
+        Rectangle, 
+        Triangle,
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
